496d857785b561eb6ecc546a683ba26f04460a42
Freeze16 <gnetov.i@mail.ru>
2023-10-26 16:58:23 +0300

    .

--- zxcdron_app/admin.py
+++ zxcdron_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Drone, Places
 
-# Register your models here.
+
+
--- zxcdron_app/models.py
+++ zxcdron_app/models.py
@@ -1,22 +1,22 @@
-# from django.db import models
-#
-#
-# class Drone(models.Model):
-#     drone_id = models.CharField(max_lenght=100)
-#     drone_is_free = models.BooleanField()
-#     power = models.FloatField()
-#     weight = models.CharField()
-#
-#     def __str__(self):
-#         return self.drone_id
-#
-#
-# class Places(models.Model):
-#     place_id = models.CharField()
-#     number_of_drones = models.IntegerField()
-#     number_of_free_drones = models.IntegerField()
-#
-#     def __str__(self):
-#         return self.place_id
+from django.db import models
+
+
+class Drone(models.Model):
+    drone_id = models.CharField(max_lenght=100)
+    drone_is_free = models.BooleanField()
+    power = models.FloatField()
+    weight = models.CharField()
+
+    def __str__(self):
+        return self.drone_id
+
+
+class Places(models.Model):
+    place_id = models.CharField()
+    number_of_drones = models.IntegerField()
+    number_of_free_drones = models.IntegerField()
+
+    def __str__(self):
+        return self.place_id
 
 

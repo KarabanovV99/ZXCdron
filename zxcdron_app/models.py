317393375dd3c28603a117c2753f4a0ade3b5737
Freeze16 <gnetov.i@mail.ru>
2023-10-26 16:59:51 +0300

    .

--- zxcdron_app/models.py
+++ zxcdron_app/models.py
@@ -2,17 +2,17 @@ from django.db import models
 
 
 class Drone(models.Model):
-    drone_id = models.CharField(max_lenght=100)
+    drone_id = models.CharField(max_length=100)
     drone_is_free = models.BooleanField()
     power = models.FloatField()
-    weight = models.CharField()
+    weight = models.CharField(max_length=100)
 
     def __str__(self):
         return self.drone_id
 
 
 class Places(models.Model):
-    place_id = models.CharField()
+    place_id = models.CharField(max_length=100)
     number_of_drones = models.IntegerField()
     number_of_free_drones = models.IntegerField()
 
